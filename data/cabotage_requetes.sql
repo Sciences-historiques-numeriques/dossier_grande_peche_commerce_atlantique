d184ed63c24ab08646e0f26b3c64231bb7f56f95
fberetta <fr.beretta@bluewin.ch>
2021-12-19 17:44:14 +0100

    Requêtes complétées

--- data/cabotage_requetes.sql
+++ data/cabotage_requetes.sql
@@ -10,8 +10,29 @@ FROM navigations n
 LIMIT 100 OFFSET 200;
 
 -- Première année, dernière année, nombre de navigations
-SELECT min(nANNEE), max(nANNEE), count(*)
-FROM navigations n ;
+SELECT min(aANNEE), max(aANNEE), count(*)
+FROM activitesbateaux a ;
+
+
+-- aucun bateau utilisé deux fois pour une année
+SELECT aNOMBATEAU, aANNEE, count(*)
+FROM activitesbateaux
+GROUP BY aNOMBATEAU , aANNEE 
+HAVING count(*) > 1 ;
+
+-- les bateaux les plus utilisés
+SELECT aNOMBATEAU, min(aANNEE), max(aANNEE), count(*) as eff
+FROM activitesbateaux
+GROUP BY aNOMBATEAU
+ORDER BY eff DESC ;
+
+
+-- les armateurs les plus actifs et leurs bateaux
+SELECT aARMEMENT , min(aANNEE), max(aANNEE), count(*) as eff, (GROUP_CONCAT(DISTINCT aNOMBATEAU))
+FROM activitesbateaux
+--WHERE aNOMBATEAU = 'ST FRANÇOIS '
+GROUP BY aARMEMENT 
+ORDER BY eff DESC ;
 
 
 
@@ -39,23 +60,75 @@ LIMIT 10;
 
 
 
---- Regrouper les trajets par bateau et année, avec lieu de départ, arrivée, nombre étapes et frets
-WITH tw1 as (SELECT nDEPART, region_depart, nNOMBATEAU, nANNEE 
-FROM v_navigations_avec_regions n
+---- Requête jointure entre voyages 8appelés 'activitésbateaux' et étapes (appelées 'navigations')
+--      Noter qu'il manque un lien direct dans le modèle et dans la base de données entre les voyages 
+--         et les étapes de chaque voyage  
+ 
+---   Création de la vue correspondante 
+DROP VIEW IF EXISTS v_voyages_etapes ;
+CREATE VIEW v_voyages_etapes AS
+SELECT a.*, n.pk_navigations, n.nNUMETAP, n.nDEPART, n.region_depart, n.nARRIVEE, n.region_arrivee, n.nFRET 
+FROM activitesbateaux a, v_navigations_avec_regions n
+WHERE a.aNOMBATEAU = n.nNOMBATEAU
+AND a.aANNEE = n.nANNEE 
+ORDER BY a.aANNEE, a.aNOMBATEAU, n.nNUMETAP ; 
+
+
+SELECT *
+FROM v_voyages_etapes
+LIMIT 50;
+
+
+--- Regrouper trajets par bateau et année, avec lieu de départ, arrivée, nombre étapes et frets
+WITH tw1 as (SELECT nDEPART, region_depart, aNOMBATEAU, aANNEE, aARMEMENT
+FROM v_voyages_etapes n
 WHERE nNUMETAP = 1
-),tw2 as (SELECT nDEPART, region_depart, nNOMBATEAU, nANNEE  
-FROM v_navigations_avec_regions n
+),tw2 as (SELECT nDEPART, region_depart, aNOMBATEAU, aANNEE  
+FROM v_voyages_etapes n
 WHERE nNUMETAP = 100
-), tw3 as (SELECT nNOMBATEAU, nANNEE, count(*) - 1 as eff_etapes,  group_concat(nFRET) as frets
-FROM navigations n
-GROUP BY  nNOMBATEAU , nANNEE
+), tw3 as (SELECT aNOMBATEAU, aANNEE, count(*) - 1 as eff_etapes,  group_concat(nFRET) as frets
+FROM v_voyages_etapes n
+GROUP BY aNOMBATEAU , aANNEE
 )
 SELECT tw1.nDEPART AS depart, tw1.region_depart as reg_depart, tw2.nDEPART AS arrivee, tw2.region_depart as reg_arrivee, 
-tw1.nANNEE, tw1.nNOMBATEAU, eff_etapes, frets
+tw1.aANNEE, tw1.aNOMBATEAU, tw1.aARMEMENT, eff_etapes, frets
 FROM tw1, tw2, tw3
-WHERE tw1.nNOMBATEAU = tw2.nNOMBATEAU
-AND tw1.nANNEE = tw2.nANNEE
-AND tw3.nNOMBATEAU = tw2.nNOMBATEAU
-AND tw3.nANNEE = tw2.nANNEE
-GROUP BY tw1.nDEPART, tw2.nDEPART, tw1.nANNEE, tw1.nNOMBATEAU
-ORDER BY tw1.nANNEE, tw1.nNOMBATEAU ;
+WHERE tw1.aNOMBATEAU = tw2.aNOMBATEAU
+AND tw1.aANNEE = tw2.aANNEE
+AND tw3.aNOMBATEAU = tw2.aNOMBATEAU
+AND tw3.aANNEE = tw2.aANNEE
+GROUP BY tw1.nDEPART, tw2.nDEPART, tw1.aANNEE, tw1.aNOMBATEAU
+ORDER BY tw1.aANNEE, tw1.aNOMBATEAU ;
+
+
+----  Requête pour exploitation dans notebook: 
+--  Regrouper trajets par bateau et année, avec lieu de départ, arrivée, nombre étapes et frets ||| puis ajouter les escales
+
+WITH tw1 as (SELECT nDEPART, region_depart, aNOMBATEAU, aANNEE, aARMEMENT
+FROM v_voyages_etapes n
+WHERE nNUMETAP = 1
+),tw2 as (SELECT nDEPART, region_depart, aNOMBATEAU, aANNEE  
+FROM v_voyages_etapes n
+WHERE nNUMETAP = 100
+), tw3 as (SELECT aNOMBATEAU, aANNEE, count(*) - 1 as eff_etapes,  group_concat(nFRET) as frets
+FROM v_voyages_etapes n
+GROUP BY aNOMBATEAU , aANNEE
+), tw4 AS (
+SELECT tw1.nDEPART AS depart, tw1.region_depart as reg_depart, tw2.nDEPART AS arrivee, tw2.region_depart as reg_arrivee, 
+tw1.aANNEE, tw1.aNOMBATEAU, tw1.aARMEMENT, eff_etapes, frets
+FROM tw1, tw2, tw3
+WHERE tw1.aNOMBATEAU = tw2.aNOMBATEAU
+AND tw1.aANNEE = tw2.aANNEE
+AND tw3.aNOMBATEAU = tw2.aNOMBATEAU
+AND tw3.aANNEE = tw2.aANNEE
+GROUP BY tw1.nDEPART, tw2.nDEPART, tw1.aANNEE, tw1.aNOMBATEAU
+ORDER BY tw1.aANNEE, tw1.aNOMBATEAU )
+SELECT s.sSTATUT, tw4.*,  n2.nNUMETAP, n2.nDEPART, n2.nARRIVEE, n2.nFRET 
+FROM tw4, navigations n2, statutsbateaux s 
+WHERE n2.nNOMBATEAU = tw4.aNOMBATEAU
+AND n2.nANNEE  = tw4.aANNEE
+AND n2.nNUMETAP != 100
+AND s.sNOMBATEAU = n2.nNOMBATEAU 
+--- AND aNOMBATEAU = 'ST FRANÇOIS '
+ORDER BY n2.nANNEE, n2.nNOMBATEAU , n2.nNUMETAP ;
+
